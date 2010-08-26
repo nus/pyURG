6303d77e1cdd69427a2e1c445956c6ec37e85405
Yota Ichino <nusichino@users.sourceforge.jp>
2010-08-26 23:34:56 +0900

    Added comments.

--- sample_pyurg.py
+++ sample_pyurg.py
@@ -3,13 +3,19 @@
 
 import pyurg
 
+# For initializing.
 urg = pyurg.UrgDevice()
 
+# Connect to the URG device.
+# If could not conncet it, get False from urg.connect()
 if not urg.connect():
     print 'Could not connect.'
     exit()
 
+# Get length datas and timestamp.
+# If missed, get [] and -1 from urg.capture()
 data, timestamp = urg.capture()
 
+# Print lengths.
 for length in data:
     print length
